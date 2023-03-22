931c9093d4734af51979228fe97b111573d7ba73
gcz <gcz1543910316@gmail.com>
2023-03-22 23:49:33 +0800

    init

---
+++ src/test/java/com/jensir/cloud/microservicesimple/MicroserviceSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jensir.cloud.microservicesimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceSimpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
